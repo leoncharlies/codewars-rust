0f5c64e175ec4367bdd67a75c0d26289f131955a
Somnus <chen1921460502@outlook.com>
2025-12-03 14:01:18 +0800

    添加新的题目“Regex validate PIN code”，更新题目列表和统计信息

--- katas/6kyu/001_unique_in_order_54e6533c92449cc251001667/src/lib.rs
+++ katas/6kyu/001_unique_in_order_54e6533c92449cc251001667/src/lib.rs
@@ -4,13 +4,32 @@
 //! Rank: 6kyu
 //! Completed: 2025-12-02
 
-pub fn solution() {
-    unimplemented!()
+fn unique_in_order<T>(sequence: T) -> Vec<T::Item>
+where
+    T: std::iter::IntoIterator,
+    T::Item: std::cmp::PartialEq + std::fmt::Debug,
+{
+    let mut result = Vec::new();
+    for item in sequence {
+        if result.last() != Some(&item) {
+            result.push(item);
+        }
+    }
+    result
 }
+
+// Add your tests here.
+// See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // 在这里粘贴从 Codewars 复制的测试代码
+    #[test]
+    fn sample_test() {
+        assert_eq!(
+            unique_in_order("AAAABBBCCDAABBB".chars()),
+            vec!['A', 'B', 'C', 'D', 'A', 'B']
+        );
+    }
 }
---
+++ katas/7kyu/001_regex_validate_pin_code_55f8a9c06c018a0d6e000132/src/lib.rs
@@ -0,0 +1,45 @@
+//! Title: Regex validate PIN code
+//! Link: https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+//! Kata ID: 55f8a9c06c018a0d6e000132
+//! Rank: 7kyu
+//! Completed: 2025-12-03
+
+fn validate_pin(pin: &str) -> bool {
+    ((pin.len() == 4) || (pin.len() == 6)) && pin.chars().all(|c| c.is_digit(10))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::validate_pin;
+
+    #[test]
+    fn invalid_length_tests() {
+        assert_eq!(validate_pin("1"), false);
+        assert_eq!(validate_pin("12"), false);
+        assert_eq!(validate_pin("123"), false);
+        assert_eq!(validate_pin("12345"), false);
+        assert_eq!(validate_pin("1234567"), false);
+        assert_eq!(validate_pin("-1234"), false);
+        assert_eq!(validate_pin("1.234"), false);
+        assert_eq!(validate_pin("00000000"), false);
+    }
+
+    #[test]
+    fn non_digit_chars_tests() {
+        assert_eq!(validate_pin("a234"), false);
+        assert_eq!(validate_pin(".234"), false);
+        assert_eq!(validate_pin("-1.234"), false);
+    }
+
+    #[test]
+    fn valid_pin_tests() {
+        assert_eq!(validate_pin("1234"), true);
+        assert_eq!(validate_pin("0000"), true);
+        assert_eq!(validate_pin("1111"), true);
+        assert_eq!(validate_pin("123456"), true);
+        assert_eq!(validate_pin("098765"), true);
+        assert_eq!(validate_pin("000000"), true);
+        assert_eq!(validate_pin("123456"), true);
+        assert_eq!(validate_pin("090909"), true);
+    }
+}
